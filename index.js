2a21afba936e0a44061ce5f433da1a1ef3abe212
fpsparkdev-creator <fp.spark.dev@gmail.com>
2026-05-07 14:54:40 +0530

    fix bugs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 console.log(process.env.EMAIL_USER, process.env.EMAIL_PASS);
@@ -11,15 +11,19 @@ app.use(cors());
 app.use(express.json());
 
 // routes
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Logiflow API");
+});
 app.use("/api", userRoutes);
 
 // app.listen(process.env.PORT, () => {
 //   console.log(`Server running on port ${process.env.PORT}`);
 // });
+module.exports = app;
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-        console.log("Server is running at : " + PORT);
-    });
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log("Server is running at : " + PORT);
+  });
 }
